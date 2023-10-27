c052a5a31ec0a0259d3ab4631463fc5abbef1fe7
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-10-27 20:29:26 +0530

    Add nltk-sentiment-analyzer submodule

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+import nltk_sentiment_analyzer.sentiment as sentiment_analyzer
 
 load_dotenv()
 
@@ -14,7 +15,7 @@ CORS(app, resources={r"/*": {"origins": host}})
 
 @app.route("/")
 def home():
-    return "Hello World"
+    return sentiment_analyzer.run()
 
 
 if __name__ == "__main__":
